2c312de565d751106e7d832b7a76178aaf8735bc
afgman4 <afgman4@naver.com>
2021-12-14 06:20:30 +0900

    init

---
+++ src/main/java/com/example/jwt/auth/PrincipalDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.jwt.auth;
+
+import com.example.jwt.model.User;
+import com.example.jwt.model.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+// http://localhost:8080/login
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("PrincipalDetailsService의 loadUserByUsername이 실행");
+        User userEntity = userRepository.findByUsername(username);
+        return new PrincipalDetails(userEntity);
+    }
+}
